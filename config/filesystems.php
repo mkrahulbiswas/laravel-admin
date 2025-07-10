38307bbaccf0446205d471ac8224936e444bbdfa
mkrahulbiswas <biswas.rahul31@gmail.com>
2025-07-10 17:27:28 +0530

    Done (10-07-2025) -2

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,6 +67,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+            'scheme'  => 'http',
         ],
 
     ],
